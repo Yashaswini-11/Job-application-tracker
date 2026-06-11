fb6a7b1a6b1e18a707a04ec043f8d5b9fa50fbe1
Yashaswini-11 <yashudn10@gmail.com>
2026-06-11 15:33:12 +0530

    debug auth

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -4,10 +4,14 @@ const auth = (req, res, next) => {
 
     const token = req.header("Authorization");
 
+    console.log("TOKEN RECEIVED:", token);
+
     if (!token) {
+
         return res.status(401).json({
             message: "No token, access denied"
         });
+
     }
 
     try {
@@ -17,17 +21,22 @@ const auth = (req, res, next) => {
             process.env.JWT_SECRET
         );
 
+        console.log("DECODED:", decoded);
+
         req.user = decoded;
 
         next();
 
     } catch (error) {
 
-        res.status(401).json({
+        console.log("AUTH ERROR:", error.message);
+
+        return res.status(401).json({
             message: "Invalid token"
         });
 
     }
+
 };
 
 module.exports = auth;
